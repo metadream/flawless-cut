0b129ffdde37f82c1b4e2029db051bb49e3a04fc
metadream <mophisoft@gmail.com>
2025-11-20 09:53:01 +0800

    refactor

--- src/renderer.js
+++ src/renderer.js
@@ -2,6 +2,9 @@
 import { $ } from "./utils.js";
 
 const fileChooser = $('#file-chooser');
+const toast = $('.toast');
+const loading = $('.loading');
+
 // const video = new Video();
 
 /* --------------------------------------------------------
@@ -33,3 +36,32 @@ fileChooser.onclick = async function() {
         video.setPath(filePaths[0]);
     }
 }
+
+/* --------------------------------------------------------
+ * Web Components
+ * ----------------------------------------------------- */
+
+Object.assign(window, {
+    toast(text) {
+        toast.message = toast.querySelector('div');
+        toast.message.innerHTML = text;
+        toast.message.classList.add('visible');
+
+        // Auto hide
+        if (toast.timer) clearTimeout(toast.timer);
+        toast.timer = setTimeout(function() {
+            toast.message.classList.remove('visible');
+        }, 3000);
+    },
+
+    loading(progress) {
+        loading.pointer = loading.querySelector('.pointer');
+        if (progress === false || progress === 100) {
+            loading.style.display = 'none';
+            loading.pointer.innerHTML = '';
+        } else {
+            loading.style.display = 'block';
+            loading.pointer.innerHTML = Number.isInteger(progress) ? progress : '';
+        }
+    }
+});
